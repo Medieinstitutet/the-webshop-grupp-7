565efb2854c58859ad47a0bbe649e58a7bdfb15b
sh-niklas <sh-niklas@hotmail.com>
2023-12-28 18:48:16 +0100

    Förberett för navigering

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,2 +1,20 @@
 import "./../scss/style.scss";
+import { Product } from "./models/Product";
+import { getProducts } from "./services/productService";
 
+async function getProductsToClass():Promise<Product[]>{
+
+    let products = await getProducts();
+    //console.log(products);
+    
+    let listOfProducts = products.map((product) => {
+        return new Product(product.id,product.title,product.price.toString(),product.description,product.category,product.image);
+    })
+    //console.log(listOfProducts);
+    return listOfProducts;
+}
+
+let products:Product[] = await getProductsToClass();
+console.log(products);
+
+localStorage.setItem("productsFromApi",JSON.stringify(products));
--- src/ts/mainProductPage.ts
+++ src/ts/mainProductPage.ts
@@ -2,30 +2,11 @@ import "./../scss/style.scss";
 import "./../scss/productPage.scss";
 import { createHtmlForProducts } from "./createHtmlForProducts";
 import { Product } from "./models/Product";
-import { getProducts } from "./services/productService";
-import { createHtmlForEachCategory } from "./createHtmlForEachCategory";
+//import { createHtmlForEachCategory } from "./createHtmlForEachCategory";
 
-async function getProductsToClass():Promise<Product[]>{
-
-    let products = await getProducts();
-    //console.log(products);
-    
-    let listOfProducts = products.map((product) => {
-        return new Product(product.id,product.title,product.price.toString(),product.description,product.category,product.image);
-    })
-    //console.log(listOfProducts);
-    return listOfProducts;
-}
-
-let products:Product[] = await getProductsToClass();
+let products:Product[] = JSON.parse(localStorage.getItem("productsFromApi") || JSON.stringify([]));
 console.log(products);
+
+// let category = localStorage.getItem("categoryName") || "";
+// createHtmlForEachCategory(products, category);
 createHtmlForProducts(products);
-
-// let test = document.createElement("h1");
-// test.innerHTML = "Test";
-// let main = document.getElementById("mainProducts");
-// main?.appendChild(test);
-
-// test.addEventListener("click", () => {
-//     createHtmlForEachCategory(products,"men's clothing");
-// })
