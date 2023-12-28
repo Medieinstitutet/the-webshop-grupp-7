e370b71809dcbd6332f5b0919b1072e821409d90
WilliamWibeck <william.wibeck@gmail.com>
2023-12-28 19:51:15 +0100

    navigering till olika kategorier klar

--- src/ts/mainProductPage.ts
+++ src/ts/mainProductPage.ts
@@ -4,9 +4,17 @@ import { createHtmlForProducts } from "./createHtmlForProducts";
 import { Product } from "./models/Product";
 //import { createHtmlForEachCategory } from "./createHtmlForEachCategory";
 
-let products:Product[] = JSON.parse(localStorage.getItem("productsFromApi") || JSON.stringify([]));
+let products: Product[] = JSON.parse(
+  localStorage.getItem("productsFromApi") || JSON.stringify([])
+);
 console.log(products);
 
-// let category = localStorage.getItem("categoryName") || "";
-// createHtmlForEachCategory(products, category);
-createHtmlForProducts(products);
+const searchParams = new URLSearchParams(window.location.search);
+
+const category = searchParams.get("category");
+
+let filteredProducts = category
+  ? products.filter((value) => category === value.category)
+  : products;
+
+createHtmlForProducts(filteredProducts);
