ef0f6a9a24e5272b7eda7b47e9c74daa913d8ced
bea0f <beatricefriberg@hotmail.com>
2023-12-29 10:47:46 +0100

    :construction: payments/checkout

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,16 +1,52 @@
 import "./../scss/style.scss";
+import "./../scss/productPage.scss";
 import { Product } from "./models/Product";
 const productsDisplay = document.getElementById("products-display");
 
 //lägg till i localStorage
-let shoppingCart: Product[] = [];
+let shoppingCart: Product[] = JSON.parse(
+  localStorage.getItem("shoppingCart") || JSON.stringify([])
+);
+
+console.log(shoppingCart);
 
 window.addEventListener("load", () => {
   if (shoppingCart.length === 0) {
     const emptyCart = document.createElement("p");
     emptyCart.innerHTML = "Din varukorg är tom";
     productsDisplay?.appendChild(emptyCart);
-  }else {
+  } else {
+    
+    let partSum:number = 0;
 
+    for (let i = 0; i < shoppingCart.length; i++) {
+      let product = document.createElement("div");
+      product.className = "product";
+      let imgBox = document.createElement("div");
+      imgBox.className = "imageContainer";
+      let img = document.createElement("img");
+      let title = document.createElement("h4");
+      let price = document.createElement("h3");
+
+      title.innerHTML = shoppingCart[i].title;
+      price.innerHTML = "$" + shoppingCart[i].price;
+      img.src = shoppingCart[i].image;
+      img.alt = "Picture of " + shoppingCart[i].title;
+
+      let x = shoppingCart[i].price;
+      let y :number = +x;
+      partSum = partSum + y;
+      
+
+
+      productsDisplay?.appendChild(product);
+      product.appendChild(imgBox);
+      imgBox.appendChild(img);
+      product.appendChild(title);
+      product.appendChild(price);
+    }
+
+    console.log(partSum);
+    
   }
 });
